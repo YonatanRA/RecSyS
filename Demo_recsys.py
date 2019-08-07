685cd1629506a7a7baf066f5c55ca7fa0e208888
YonatanRA <yeho@mail.com>
2019-08-07 11:46:01 +0200

    demo

---
+++ Demo_recsys.py
@@ -0,0 +1,20 @@
+# Demo recsys
+
+
+
+# import libraries
+
+import warnings
+warnings.filterwarnings("ignore",category=DeprecationWarning)
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.spatial.distance import pdist, squareform
+
+
+
+
+
+
+
+
