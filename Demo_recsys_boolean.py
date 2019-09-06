86f6d5df827feb714fb857756a5182f661882ebf
YonatanRA <yeho@mail.com>
2019-09-06 12:36:38 -0500

    RecSys

--- Demo_recsys_boolean.py
+++ Demo_recsys_boolean.py
@@ -46,7 +46,6 @@ def new_user(df, rb, rock, jazz, techno, pop, indie, cinema, theater, beers, win
           'cinema', 'theater', 'beers', 'wine', 'party', 'trips',
           'running', 'gym', 'golf', 'basket', 'football', 'yoga']
      
-     
     rating=[rb, rock, jazz, techno, pop, indie, cinema, theater, beers, wine,
              party, trips, running, gym, golf, basket, football, yoga]
 
