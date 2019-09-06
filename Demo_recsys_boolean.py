da017f50d9756abc0e1ca97d68e780413b1a3bff
YonatanRA <yeho@mail.com>
2019-09-06 12:31:22 -0500

    RecSys

--- Demo_recsys_boolean.py
+++ Demo_recsys_boolean.py
@@ -50,6 +50,7 @@ def new_user(df, rb, rock, jazz, techno, pop, indie, cinema, theater, beers, win
     rating=[rb, rock, jazz, techno, pop, indie, cinema, theater, beers, wine,
              party, trips, running, gym, golf, basket, football, yoga]
            
+           
     
     
     n_user={k:v for k, v in list(zip(tags, rating))}
